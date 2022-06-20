8ec17f590dd3929047ad6336bb0d74bd9a0578e4
Tair00 <suleimanov.t.i.20@gmail.com>
2022-06-20 16:55:58 +0300

    initiale project

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,23 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* Страница Донателло */
+router.get('/don', function(req, res, next) {
+  res.send("<h1>Страница Донателло</h1>")
+});
 
+/* Страница Рафаэль */
+router.get('/raf', function(req, res, next) {
+  res.send("<h1>Страница Рафаэля </h1>")
+});
+
+/* Страница Микеланджело */
+router.get('/mike', function(req, res, next) {
+  res.send("<h1>Страница Микеланджело</h1>")
+});
+
+/* Страница Леонардо */
+router.get('/leo', function(req, res, next) {
+  res.send("<h1>Страница Леонардо</h1>")
+});
 module.exports = router;
