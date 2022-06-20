410b99b1b79e6db033574db0c89a6a644cdacf2d
Tair00 <suleimanov.t.i.20@gmail.com>
2022-06-20 18:49:47 +0300

    br_4_3

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,38 @@ router.get('/', function(req, res, next) {
 });
 /* Страница Донателло */
 router.get('/don', function(req, res, next) {
-  res.send("<h1>Страница Донателло</h1>")
+  res.render('hero', {
+      title: "Донателло",
+      picture: "images/don.jpg",
+      desc: "Наш парень, с технологиями на ты, при помощи Scratch и калькулятора способен создать атомный манипулятор"
+  });
 });
+
 
 /* Страница Рафаэль */
 router.get('/raf', function(req, res, next) {
-  res.send("<h1>Страница Рафаэля </h1>")
+  res.render('hero', {
+      title: " Рафаэль ",
+      picture: "images/raf.jpg",
+      desc: "Немного нервный, но все же добрый и справедливый чело... чепепаха"
+  });
 });
 
 /* Страница Микеланджело */
 router.get('/mike', function(req, res, next) {
-  res.send("<h1>Страница Микеланджело</h1>")
+  res.render('hero', {
+      title: "Микеланджело ",
+      picture: "images/mike.jpg",
+      desc: "Весельчак, рядом с ним всегда происходит что то странное"
+  });
 });
 
 /* Страница Леонардо */
 router.get('/leo', function(req, res, next) {
-  res.send("<h1>Страница Леонардо</h1>")
+  res.render('hero', {
+      title: "Леонардо",
+      picture: "images/leo.jpg",
+      desc: "Прирожденный лидер"
+  });
 });
 module.exports = router;
