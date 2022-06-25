2658c106db39edc91209ab7caf017fb611277bbe
Tair00 <suleimanov.t.i.20@gmail.com>
2022-06-25 15:25:49 +0300

    br_6_1

---
+++ createDB.js
@@ -0,0 +1,9 @@
+var MongoClient = require("mongodb").MongoClient
+
+MongoClient.connect("mongodb://localhost:27017/all", function(err,db){
+    if(err) throw err
+    var collection = db.collection("hero")
+    collection.insertOne({name:"Леонардо"},function(err,result){
+        db.close()
+    })
+})
