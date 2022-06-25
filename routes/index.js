6fa1fad0eb1cfd3ab9fc9bb827c5b3e453893abb
Tair00 <suleimanov.t.i.20@gmail.com>
2022-06-25 21:40:13 +0300

    br_11_2

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+
 var express = require('express')
 var router = express.Router()
 var Hero = require("../models/hero").Hero
@@ -5,13 +6,11 @@ var Hero = require("../models/hero").Hero
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
-        res.render('index', {
-                                title: 'Express',
-                                menu: menu
-                            });
-    })
-
+  res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu:menu });
 });
+
+
+
+
 
 module.exports = router;
