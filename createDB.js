5aa017d1a4982944c2f77c0dcee70945552e3aa1
Tair00 <suleimanov.t.i.20@gmail.com>
2022-06-25 15:32:30 +0300

    br_6_2

--- createDB.js
+++ createDB.js
@@ -1,4 +1,5 @@
 var MongoClient = require("mongodb").MongoClient
+var data = require("./data.js").data
 
 MongoClient.connect("mongodb://localhost:27017/all", function(err,db){
     if(err) throw err
---
+++ data.js
@@ -0,0 +1,28 @@
+
+var data = [{
+    title: ' Рафаэль ',
+    nick: 'raf',
+    avatar: 'images/raf.jpg',
+    desc: 'Немного нервный, но все же добрый и справедливый чело... чепепаха'
+},
+{
+    title: 'Микеланджело',
+    nick: 'mike',
+    avatar: 'images/mike.jpg',
+    desc: 'Весельчак, рядом с ним всегда происходит что то странное'
+},
+{
+    title: 'Леонардо',
+    nick: 'leo',
+    avatar: 'images/leo.jpg',
+    desc: 'Прирожденный лидер'
+},
+{
+    title: 'Донателло',
+    nick: 'don',
+    avatar: 'images/don.jpg',
+    desc: 'Наш парень, с технологиями на ты, при помощи Scratch и калькулятора способен создать атомный манипулятор'
+},
+];
+
+module.exports.data = data;
