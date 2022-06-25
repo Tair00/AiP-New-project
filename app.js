35126f11ce70b4f078654bba352c4ea299840723
Tair00 <suleimanov.t.i.20@gmail.com>
2022-06-25 20:37:59 +0300

    br_10_2

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var heroes = require('./routes/heroes');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var heroes = require('./routes/heroes');
 var app = express();
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/all')
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/heroes', heroes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/heroes.js
+++ routes/heroes.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маошрутов, начинающихся с heroes');
+  res.send('respond with a resource');
 });
-
+router.get("/:nick", function(req, res, next) {
+    res.send(req.params.nick);
+});
 module.exports = router;
