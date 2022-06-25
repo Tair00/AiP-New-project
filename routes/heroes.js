5aa7d23c2eee4734ac21809a09a1f3cef81a6b14
Tair00 <suleimanov.t.i.20@gmail.com>
2022-06-25 20:42:04 +0300

    br_10_3

--- routes/heroes.js
+++ routes/heroes.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+var Hero = require("../models/hero").Hero
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-});
+router.get('/:nick', function(req, res, next) {
+    Hero.findOne({nick:req.params.nick}, function(err,hero){
+        if(err) return next(err)
+        if(!hero) return next(new Error("Нет такого героя в этой книжке"))
+        res.render('hero', {
+            title: hero.title,
+            picture: hero.avatar,
+            desc: hero.desc
+        })
+    })
+})
+
 module.exports = router;
