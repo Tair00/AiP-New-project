f1c53bb6ca910edabe0cd113773094ebaec12b8a
Tair00 <suleimanov.t.i.20@gmail.com>
2022-06-25 15:51:55 +0300

    br_8_2

--- createDB.js
+++ createDB.js
@@ -1,24 +1,14 @@
-// var MongoClient = require("mongodb").MongoClient
-// var data = require("./data.js").data
-
-// MongoClient.connect("mongodb://localhost:27017/all", function(err,db){
-//     if(err) throw err
-//     db.dropDatabase()
-//     var collection = db.collection("hero")
-//     collection.insertMany(data,function(err,result){
-//         db.close()
-//     })
-// })
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/test')
+mongoose.connect('mongodb://localhost/test');
 
-var Cat = mongoose.model('Cat', { name: String })
+var schema = mongoose.Schema({ name: String })
+var Cat = mongoose.model('Cat', schema);
 
-var kitty = new Cat({ name: 'Пушок' })
+var kitty = new Cat({ name: 'Пушок' });
 kitty.save(function (err) {
     if (err) {
-        console.log(err)
+        console.log(err);
     } else {
-        console.log('Мяу')
+        console.log('Мяу');
     }
-})
+});
