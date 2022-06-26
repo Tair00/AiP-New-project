cf1412a5676bfce765ae200af3ce4c4b64cb9f11
Tair00 <suleimanov.t.i.20@gmail.com>
2022-06-26 14:35:44 +0300

    br_12_1

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(function(req,res,next){
   })
 })
 
-// app.use(require("./middleware/createMenu.js"))
+app.use(require("./middleware/createMenu.js"))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,19 @@ var express = require('express')
 var router = express.Router()
 var Hero = require("../models/hero").Hero
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
     Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
         req.session.greeting = "Hi!!!"
-        res.render('index', { title: 'Express',  
+        res.render('index', { title: 'Express',
         counter:req.session.counter });
     })
 
 });
+
+router.get('/logreg', function(req, res, next) {
+    res.render('logreg',{title: 'Вход'});
+});   
+
 
 module.exports = router;
